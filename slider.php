1bc4eb5814c89896605f00da9dcdba1529525239
murugesh <kmurugesh.mca@gmail.com>
2021-05-15 21:11:16 +0530

    PHP Conversion

---
+++ slider.php
@@ -0,0 +1,27 @@
+<section class="home-slider owl-carousel">
+      <div class="slider-item" style="background-image:url(images/bg_1.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 ftco-animate">
+            <h1 class="mb-4">Excellent Education Needs Complete Solution</h1>
+            <p></p>
+            <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="slider-item" style="background-image:url(images/bg_2.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 ftco-animate">
+            <h1 class="mb-4">University, College School Education</h1>
+            <p></p>
+            <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
